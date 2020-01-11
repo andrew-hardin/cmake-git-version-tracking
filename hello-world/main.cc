e26cfd6162f3da2b0510b2fda7c59741fa4a735e
Andrew Hardin <andrew.hardin5+git@gmail.com>
2020-01-11 15:07:08 -0700

    Track the commit author, email, date, subject and notes.

--- hello-world/main.cc
+++ hello-world/main.cc
@@ -6,8 +6,14 @@
 int main() {
     // Demo that our macros work.
     if(GIT_RETRIEVED_STATE) {
-        std::cout << "INFO: " << GIT_HEAD_SHA1 << std::endl;
         if(GIT_IS_DIRTY) std::cerr << "WARN: there were uncommitted changes." << std::endl;
+
+        // Print information about the commit.
+        // The format imitates the output from "git log".
+        std::cout << "\n\ncommit " << GIT_HEAD_SHA1 << " (HEAD)\n"
+                  << "Author: " << GIT_AUTHOR_NAME << " <" << GIT_AUTHOR_EMAIL << ">\n"
+                  << "Date: " << GIT_COMMIT_DATE_ISO8601 << "\n\n"
+                  << GIT_COMMIT_SUBJECT << "\n" << GIT_COMMIT_NOTES << std::endl;
         return EXIT_SUCCESS;
     }
     else {
