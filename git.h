6c2092994233d456fea20918290363a233529c7d
Andrew Hardin <andrew.hardin5+git@gmail.com>
2022-07-24 22:21:29 -0600

    Require C99; add optional CXX98 and CXX17 interfaces.

--- git.h
+++ git.h
@@ -1,31 +1,140 @@
 #pragma once
+// git.h
+// https://raw.githubusercontent.com/andrew-hardin/cmake-git-version-tracking/master/git.h
+//
+// Released under the MIT License.
+// https://raw.githubusercontent.com/andrew-hardin/cmake-git-version-tracking/master/LICENSE
+
+#include <stdbool.h>
+
+/// Is the metadata populated? 
+//
+/// We may not have metadata if there wasn't a .git directory
+/// (e.g. downloaded source code without revision history).
+bool git_IsPopulated();
+
+/// Were there any uncommitted changes that won't be reflected
+/// in the CommitID?
+bool git_AnyUncommittedChanges();
+
+/// The commit author's name.
+const char* git_AuthorName();
+
+/// The commit author's email.
+const char* git_AuthorEmail();
+
+/// The commit SHA1.
+const char* git_CommitSHA1();
+
+/// The ISO8601 commit date.
+const char* git_CommitDate();
+
+/// The commit subject.
+const char* git_CommitSubject();
+
+/// The commit body.
+const char* git_CommitBody();
+
+/// The commit describe.
+const char* git_Describe();
+
+/// The symbolic reference tied to HEAD.
+const char* git_Branch();
+
+
+#ifdef __cplusplus
+
+/// This is a utility extension for C++ units.
+/// It provides a "git" namespace that wraps the
+/// C methods in more ergonomic types.
+///
+/// This is header-only in an effort to keep the
+/// underlying static library C99 compliant.
+
+
+// We really want to use std::string_view if it appears
+// that the compiler will support it. If that fails,
+/// revert back to std::string.
+#define GIT_VERSION_TRACKING_CPP_17_STANDARD 201703L
+#if __cplusplus >= GIT_VERSION_TRACKING_CPP_17_STANDARD
+#define GIT_VERSION_USE_STRING_VIEW 1
+#else
+#define GIT_VERSION_USE_STRING_VIEW 0
+#endif
+
+
+#if GIT_VERSION_USE_STRING_VIEW
+#include <cstring>
+#include <string_view>
+#else
 #include <string>
+#endif
 
-class GitMetadata {
-public:
-  // Is the metadata populated? We may not have metadata if
-  // there wasn't a .git directory (e.g. downloaded source
-  // code without revision history).
-  static bool Populated();
+namespace git {
 
-  // Were there any uncommitted changes that won't be reflected
-  // in the CommitID?
-  static bool AnyUncommittedChanges();
+#if GIT_VERSION_USE_STRING_VIEW
+using StringOrView = std::string_view;
+#else
+using StringOrView = std::string;
+#endif
 
-  // The commit author's name.
-  static std::string AuthorName();
-  // The commit author's email.
-  static std::string AuthorEmail();
-  // The commit SHA1.
-  static std::string CommitSHA1();
-  // The ISO8601 commit date.
-  static std::string CommitDate();
-  // The commit subject.
-  static std::string CommitSubject();
-  // The commit body.
-  static std::string CommitBody();
-  // The commit describe.
-  static std::string Describe();
-  // The symbolic reference tied to HEAD.
-  static std::string Branch();
-};
+namespace internal {
+
+/// Short-hand method for initializing a std::string or std::string_view given a C-style const char*.
+const StringOrView InitString(const char* from_c_interface) {
+  #if GIT_VERSION_USE_STRING_VIEW
+    return StringOrView { from_c_interface, std::strlen(from_c_interface) };
+  #else
+    return std::string(from_c_interface);
+  #endif
+}
+
+}  // namespace internal
+
+inline bool IsPopulated() {
+  return git_IsPopulated();
+}
+inline bool AnyUncommittedChanges() {
+  return  git_AnyUncommittedChanges();
+}
+inline const StringOrView& AuthorName() {
+  static const StringOrView kValue = internal::InitString(git_AuthorName());
+  return kValue;
+}
+inline const StringOrView AuthorEmail() {
+  static const StringOrView kValue = internal::InitString(git_AuthorEmail());
+  return kValue;
+}
+inline const StringOrView CommitSHA1() {
+  static const StringOrView kValue = internal::InitString(git_CommitSHA1());
+  return kValue;
+}
+inline const StringOrView CommitDate() {
+  static const StringOrView kValue = internal::InitString(git_CommitDate());
+  return kValue;
+}
+inline const StringOrView CommitSubject() {
+  static const StringOrView kValue = internal::InitString(git_CommitSubject());
+  return kValue;
+}
+inline const StringOrView CommitBody() {
+  static const StringOrView kValue = internal::InitString(git_CommitBody());
+  return kValue;
+}
+inline const StringOrView Describe() {
+  static const StringOrView kValue = internal::InitString(git_Describe());
+  return kValue;
+}
+inline const StringOrView Branch() {
+  static const StringOrView kValue = internal::InitString(git_Branch());
+  return kValue;
+}
+
+}  // namespace git
+
+
+// Cleanup our defines to avoid polluting.
+#undef GIT_VERSION_USE_STRING_VIEW
+#undef GIT_VERSION_TRACKING_CPP_17_STANDARD
+
+#endif  // __cplusplus
